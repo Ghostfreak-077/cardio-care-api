ea5b5646001120d0929bc17888e0e6cf2d065759
Partha Pratim Deka <parthapratimdeka129@gmail.com>
2024-07-18 10:19:11 +0530

    RESOLVE: import error

--- SOS_service/server.js
+++ SOS_service/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 // const corsMiddleware = require('../middleware/corsMiddleware');
-const axios = require("axios");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
